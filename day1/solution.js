793b1b9662e6aa5c3fb59e53b7f1d6f2c7424dfc
LucasMedeiros7 <lucaasmedeiros7@gmail.com>
2023-12-04 12:10:07 -0300

    refactor solution day1

--- day1/solution.js
+++ day1/solution.js
@@ -1,46 +1,44 @@
 const fs = require('fs')
 
-const mapNumbers = {
-  zero: 0,
-  one: 1,
-  two: 2,
-  three: 3,
-  four: 4,
-  five: 5,
-  six: 6,
-  seven: 7,
-  eight: 8,
-  nine: 9
+const numberMap = {
+  "zero": "ze0o",
+  "one": "on1e",
+  "two": "tw2o",
+  "three": "th3ree",
+  "four": "fo4ur",
+  "five": "fi5ve",
+  "six": "si6x",
+  "seven": "se7ven",
+  "eight": "ei8ght",
+  "nine": "ni9ne"
 };
-const rgx = /(one|two|three|four|five|six|seven|eight|nine)/g;
 
 function solution() {
   const lines = fs.readFileSync('input2.txt', 'utf8').split('\n');
-
   let sum = 0;
-  let spelledNumber = '';
-  let numbers = '';
 
   for (const line of lines) {
+    let modStr = line;
 
-    for (let i = 0; i < line.length; i++) {
-      spelledNumber += line[i];
-
-      if (spelledNumber.match(rgx)) {
-        numbers += mapNumbers[spelledNumber.match(rgx)].toString()
-        spelledNumber = ''
-        i--
-      }
-
-      if (!isNaN(line[i])) numbers += line[i]
+    for (const [key, value] in numberMap) {
+      modStr = modStr.replace(new RegExp(key, 'g'), value);
     }
 
-    let combinedString = numbers[0] + numbers[numbers.length - 1]
-    numbers = '';
-    sum += parseInt(combinedString);
+    let firstDigit = -1;
+    let lastDigit = -1;
+
+    for (const c of modStr) {
+      const digit = parseInt(c, 10);
+      if (!isNaN(digit)) {
+        firstDigit = (firstDigit === -1) ? digit * 10 : firstDigit;
+        lastDigit = digit;
+      }
+    }
+
+    sum += firstDigit + lastDigit;
   }
 
-  return sum;
+  return sum
 }
 
 console.log(solution());
